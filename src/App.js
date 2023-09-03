1abde6ea5d5c99f8d7c16eb0722df34ef8bb9fe5
DeepeshMitra <deepesh.mitra@saisonomni.com>
2023-09-03 23:42:12 +0530

    Breakpoint fixes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ function App() {
   return (
       <div className = "w-full h-screen bg-[#e9af4b] overflow-y-scroll snap-y snap-mandatory">
         <div className="w-full h-full flex items-center justify-center  snap-start px-4 py-2">
-          <div className="container h-full flex flex-col lg:flex-row gap-y-8 items-center justify-center">
+          <div className="container h-full flex flex-col lg:flex-row gap-y-8 gap-x-4 items-center justify-center">
             <div className="basis-3/5 flex flex-col gap-y-4 justify-start items-start">
-            <h1 className="text-6xl leading-normal font-bold mb-4 break-keep">
-              Lorem Ipsum is simply dummy text. Yes it is just dummy
-                </h1>
-                <p className="mb-4 2xs:max-xs:hidden font-normal leading-normal text-gray-500 ">
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                </p>
+              <h1 className="text-6xl leading-normal font-bold mb-4 break-keep">
+              Open in screen of full viewport height
+              </h1>
+              <p className="mb-4 hidden xs:block font-normal leading-normal text-gray-500 ">
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
             </div>
-            <div className="flex-grow 2xs:max-xs:hidden block overflow-hidden relative w-full h-full ">
+            <div className="flex-grow hidden xs:block overflow-hidden relative w-full h-full ">
                 <img
                   src="https://placedog.net/500/280"
                   className=" object-contain w-full h-full"
@@ -24,7 +24,7 @@ function App() {
         </div>
 
         {/* This below section shows up only when the screen size is less than 640px */}
-        <div className="w-full h-full 2xs:max-xs:flex hidden  items-center justify-center  snap-start px-4 py-2">
+        <div className="w-full h-full flex xs:hidden items-center justify-center  snap-start px-4 py-2">
           <div className="container h-full flex flex-col lg:flex-row gap-y-8 items-center justify-center">
             Showing for small screen sizes
             <div className="overflow-hidden items-center">
